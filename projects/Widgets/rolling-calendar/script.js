e9a6b0ba1da0a39ee05d4740538d1566974d116a
Bram Werink <bramwerink@gmail.com>
2024-12-22 01:53:20 +0100

    wip

--- projects/Widgets/rolling-calendar/script.js
+++ projects/Widgets/rolling-calendar/script.js
@@ -5,10 +5,12 @@ var repeater = setInterval(function(){
 var now = new Date();
 var hours = now.getHours();
 var minutes = now.getMinutes();
+var seconds = now.getSeconds();
 
-hours = hours <10 ? "0" + hours : hours
-minutes = minutes < 10 ? "0" + minutes : minutes
+hours = hours <10 ? "0" + hours : hours;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+seconds = seconds < 10 ? "0" + seconds : seconds;
 
-document.getElementById("time").innerHTML = hours + ":" + minutes;
+document.getElementById("current-time").innerHTML = hours + ":" + minutes + "<span>" + ":" + seconds + "</span>";
 }, 1000);
 
