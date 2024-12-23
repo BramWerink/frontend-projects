6e90ef73a465f9fb90407dc3ab196c9cdf7b5587
Bram Werink <bramwerink@gmail.com>
2024-12-23 22:44:44 +0100

    wip

--- projects/dropdown/script.js
+++ projects/dropdown/script.js
@@ -14,3 +14,16 @@ function saveOption(){
     console.log(savedOption);
     document.getElementById("selected-option").innerHTML = savedOption
 }
+
+let page = 0;
+setInterval(function(){
+    selectElement.value = list[page]
+    page = (page + 1);
+    document.getElementById("selected-option").innerHTML = selectElement.value;
+
+    console.log(selectElement.value);
+}, 1000);
+
+// todo
+// 1. oops, the dropdown now also changes.
+// 2. Stop looping when selection is made
