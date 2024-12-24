0fea4420c0fe0e9b9c3123c056f1f4e8da225f18
Bram Werink <bramwerink@gmail.com>
2024-12-24 10:38:44 +0100

    Added bingo effect and clear button

--- projects/dropdown/script.js
+++ projects/dropdown/script.js
@@ -1,35 +1,59 @@
-let list = ["HTML","CSS","Javascript","PHP","C#"];
+let list = ["HTML", "CSS", "Javascript", "PHP", "C#"];
 
 let selectElement = document.querySelector(".list");
 
+// Add options to the select element
 list.forEach(item => {
     let option = document.createElement("option");
     option.textContent = item;
     selectElement.appendChild(option);
 });
 
+// Initialize savedOption as an empty string
+let savedOption = "";
 
-function saveOption(){
-    let savedOption = selectElement.value
-    console.log(savedOption);
-    document.getElementById("selected-option").innerHTML = savedOption
+// Store the interval ID for clearing later
+let intervalId;
+let page = 0;
+
+// Start the bingo effect upon page load if no option is selected
+window.onload = function() {
+    if (savedOption === "") {
+        startBingoEffect(); // Start bingo effect if no option is saved
+    }
 }
 
-let page = list.length
+function saveOption() {
+    savedOption = selectElement.value; // Save the selected option
+    document.getElementById("selected-option").innerHTML = savedOption;
 
-
-function updateBingoEffect(){
-    let bingoEffect = list[page]
-    page = (page + 1) % list.length;
-    console.log(bingoEffect)
-    document.getElementById("selected-option").innerHTML = bingoEffect
+    // If an option is selected, stop the bingo effect
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
 }
 
-setInterval(updateBingoEffect, 1000);
+function startBingoEffect() {
+    // Start the bingo effect interval
+    intervalId = setInterval(updateBingoEffect, 1000);
+}
+
+function updateBingoEffect() {
+    let bingoEffect = list[page];
+    page = (page + 1) % list.length; // Move to the next item in the list
+    console.log(bingoEffect);
+    document.getElementById("selected-option").innerHTML = bingoEffect;
+}
 
 
-// if savedOption = empty{
-// setInterval(updateBingoEffect, 1000);
-// } else {  }
+function clearOption(){
+    savedOption === ""; // Clear the saved option
+    selectElement.value = "";
+    document.getElementById("selected-option").innerHTML = ""
 
+    startBingoEffect(); // Start the bingo effect again
+}
 
+// Example of a button that triggers saveOption
+// Add a button in your HTML like:
+// <button onclick="saveOption()">Save Option</button>
